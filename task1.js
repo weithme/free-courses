7fafb6e51e5e76483ed4cf9aebe3655dfc883315
weithme <39808684+weithme@users.noreply.github.com>
2018-06-07 00:32:05 +0300

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,16 @@
+var i = process.argv[2];
+/*
+var a = process.argv[3];
+var b = process.argv[4];
+
+//i = A + Ba + Caa + Daaa + ...;
+sum
+i = Z + X*b + Y*bb +...;
+
+for (var j=0;j<i.length; j++) {
+  var c = parseInt(i[j]);
+}
+var n10 = i;
+var result = n10.toString(b);
+*/
+process.stdout.write(result);
